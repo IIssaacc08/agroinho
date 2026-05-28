737ddd51bca6c8e007c7f654e47252481e30fcf0
IIssaacc08 <eurick.campos@escola.pr.gov.br>
2026-05-28 11:35:56 -0300

    código

---
+++ script.js
@@ -0,0 +1,19 @@
+AOS.init({
+
+  duration:1200,
+
+  once:true
+
+});
+
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
+
+  max:25,
+
+  speed:400,
+
+  glare:true,
+
+  "max-glare":0.5
+
+});
